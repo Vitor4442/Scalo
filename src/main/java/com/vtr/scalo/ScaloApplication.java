8a7e0ebbe065a4171a391cc8539f26010f5c08f9
José Vitor <jose.vitorw018@gmail.com>
2026-08-10 23:27:41 -0300

    First commit

---
+++ src/main/java/com/vtr/scalo/ScaloApplication.java
@@ -0,0 +1,13 @@
+package com.vtr.scalo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScaloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScaloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vtr/scalo/ScaloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vtr.scalo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScaloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
