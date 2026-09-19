0be3185e8c95c21b112acb4d5f078e29dc3e6843
José Vitor <jose.vitorw018@gmail.com>
2026-09-18 22:00:27 -0300

    Criado uma verificação para verificar o email

--- src/main/java/com/vtr/scalo/users/repository/UserRepository.java
+++ src/main/java/com/vtr/scalo/users/repository/UserRepository.java
@@ -1,6 +1,9 @@
 package com.vtr.scalo.users.repository;
 
 import com.vtr.scalo.users.entity.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +11,5 @@ import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
+    boolean existsByEmail(@NotBlank(message = "O e-mail é obrigatório") @Email(message = "E-mail inválido") @Size(max = 255, message = "O e-mail deve ter no máximo 255 caracteres") String email);
 }
