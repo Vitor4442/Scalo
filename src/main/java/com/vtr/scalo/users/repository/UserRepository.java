3570cc6d607da62244716b772bf30b277abbf53f
José Vitor <jose.vitorw018@gmail.com>
2026-09-25 23:35:36 -0300

    Metodo para retornar UserDetails

--- src/main/java/com/vtr/scalo/users/repository/UserRepository.java
+++ src/main/java/com/vtr/scalo/users/repository/UserRepository.java
@@ -5,11 +5,13 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, UUID> {
+public interface UserRepository extends JpaRepository<User, Integer> {
     boolean existsByEmail(@NotBlank(message = "O e-mail é obrigatório") @Email(message = "E-mail inválido") @Size(max = 255, message = "O e-mail deve ter no máximo 255 caracteres") String email);
+    UserDetails findByemail (String email);
 }
