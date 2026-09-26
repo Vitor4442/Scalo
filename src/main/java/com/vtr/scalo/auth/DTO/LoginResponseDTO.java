e452db79329412db5bfec8111f22f81d9614562c
José Vitor <jose.vitorw018@gmail.com>
2026-09-25 23:39:30 -0300

    Implementando a autentificação JWT

---
+++ src/main/java/com/vtr/scalo/auth/DTO/LoginResponseDTO.java
@@ -0,0 +1,4 @@
+package com.vtr.scalo.auth.DTO;
+
+public record LoginResponseDTO(String token) {
+}
--- src/main/java/com/vtr/scalo/company/dto/CompanyUserRequestDTO.java
+++ src/main/java/com/vtr/scalo/company/dto/CompanyUserRequestDTO.java
@@ -1,6 +1,11 @@
 package com.vtr.scalo.company.dto;
 
 import com.vtr.scalo.users.dto.UserRequestDto;
+import jakarta.validation.Valid;
 
-public record CompanyUserRequestDTO(CompanyRequestDTO company, UserRequestDto user) {
+public record CompanyUserRequestDTO(
+        @Valid
+        CompanyRequestDTO company,
+        @Valid
+        UserRequestDto user) {
 }
